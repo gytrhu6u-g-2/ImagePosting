32d9dcccf18b022b9e22bc12cd0c7cc32979714e
gytrhu6u-g-2 <gytrhu6u@icloud.com>
2023-05-23 21:41:28 +0900

    dbConfigにpasswordを追加

--- dbConfig.php
+++ dbConfig.php
@@ -2,9 +2,12 @@
 // DB接続ファイル
     $dbName = 'mysql:host=localhost;dbname=imageposting;charset=utf8';
     $userName = 'root';
+    $password = 'root';
 
     try {
-        $db = new PDO($dbName, $userName);
+        $db = new PDO($dbName, $userName, $password);
+        var_dump("接続成功");
     } catch (\Throwable $th) {
+        var_dump($th);
         exit();
     }
